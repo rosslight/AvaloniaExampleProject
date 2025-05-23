aed13f653ffd2a7bcea739acd0c3597e80bdb433
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-05-23 22:01:13 +0200

    test: Add sanity check for using inter font

--- test/AvaloniaExampleProject.Tests/VerifyHelpers.cs
+++ test/AvaloniaExampleProject.Tests/VerifyHelpers.cs
@@ -2,6 +2,7 @@ using System.Runtime.CompilerServices;
 using Avalonia.Controls.Primitives;
 using Avalonia.Media;
 using AvaloniaExampleProject.Assets;
+using Shouldly;
 
 namespace AvaloniaExampleProject.Tests;
 
@@ -10,6 +11,8 @@ public static class VerifyHelpers
     public static SettingsTask VerifyControl(TemplatedControl control, [CallerFilePath] string? callerFilePath = null)
     {
         control.FontFamily = new FontFamily("avares://Avalonia.Fonts.Inter/Assets/Inter-Regular.ttf#Inter");
+        control.FontFamily.Name.ShouldBe("Inter");
+        control.FontFamily.FamilyTypefaces.Count.ShouldBe(1);
         string directory =
             Path.GetFileNameWithoutExtension(callerFilePath) ?? throw new ArgumentNullException(nameof(callerFilePath));
         return Verify(control).ScrubMembersWithType<Resources>().UseDirectory(Path.Join("Snapshots", directory));
