a77c195aad84b17a9f9a110e3d875dc74f5c932a
Ole Rosskamp <ole.rosskamp@rosslight.de>
2025-05-24 09:55:36 +0200

    test: Revert to only relevant tests

--- test/AvaloniaExampleProject.Tests/ViewModels/SettingsViewModelTests.cs
+++ test/AvaloniaExampleProject.Tests/ViewModels/SettingsViewModelTests.cs
@@ -1,11 +1,7 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Headless.XUnit;
-using Avalonia.Media;
+﻿using Avalonia.Headless.XUnit;
 using AvaloniaExampleProject.Assets;
 using AvaloniaExampleProject.ViewModels;
 using AvaloniaExampleProject.Views;
-using FluentAvalonia.Styling;
 using Microsoft.Extensions.DependencyInjection;
 using static AvaloniaExampleProject.Tests.VerifyHelpers;
 
@@ -30,76 +26,4 @@ public class SettingsViewModelTests
         };
         return VerifyControl(control);
     }
-
-    [AvaloniaFact]
-    public Task Render_FluentAvalonia()
-    {
-        var control = new UserControl
-        {
-            Content = new TextBlock { Text = "This is some random text!" },
-            Foreground = new SolidColorBrush(Color.Parse("Black")),
-        };
-        control.Background = new SolidColorBrush(0xFFFFFFFF);
-        return VerifyControl(control);
-    }
-    /*
-
-    [AvaloniaFact]
-    public Task Render_Default()
-    {
-        var control = new UserControl
-        {
-            Content = new TextBlock { Text = "This is some random text!" },
-            Foreground = new SolidColorBrush(Color.Parse("Black")),
-        };
-        control.Background = new SolidColorBrush(0xFFFFFFFF);
-        var styles = Application.Current!.Styles;
-        styles.RemoveAll(styles.OfType<FluentAvaloniaTheme>());
-        return VerifyControl(control);
-    }
-
-    [AvaloniaFact]
-    public Task Render_FontSize()
-    {
-        var control = new UserControl
-        {
-            Content = new TextBlock { Text = "This is some random text!" },
-            FontSize = 14,
-            Foreground = new SolidColorBrush(Color.Parse("Black")),
-        };
-        control.Background = new SolidColorBrush(0xFFFFFFFF);
-        var styles = Application.Current!.Styles;
-        styles.RemoveAll(styles.OfType<FluentAvaloniaTheme>());
-        return VerifyControl(control);
-    }
-
-    [AvaloniaFact]
-    public Task Render_FontWeight()
-    {
-        var control = new UserControl
-        {
-            Content = new TextBlock { Text = "This is some random text!" },
-            FontWeight = FontWeight.SemiBold,
-            Foreground = new SolidColorBrush(Color.Parse("Black")),
-        };
-        control.Background = new SolidColorBrush(0xFFFFFFFF);
-        var styles = Application.Current!.Styles;
-        styles.RemoveAll(styles.OfType<FluentAvaloniaTheme>());
-        return VerifyControl(control);
-    }
-
-    [AvaloniaFact]
-    public Task Render_FontSizeAndWeight()
-    {
-        var control = new UserControl
-        {
-            Content = new TextBlock { Text = "This is some random text!" },
-            FontWeight = FontWeight.SemiBold,
-            FontSize = 14,
-        };
-        var styles = Application.Current!.Styles;
-        styles.RemoveAll(styles.OfType<FluentAvaloniaTheme>());
-        return VerifyControl(control);
-    }
-    */
 }
